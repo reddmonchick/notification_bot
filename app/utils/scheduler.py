1bc8242839eaca790ec8533fe0cbbd86d3cad4b0
reddmonchick <reddmonchick375@gmail.com>
2025-12-26 13:51:13 +0300

    fix problem with logging

--- app/utils/scheduler.py
+++ app/utils/scheduler.py
@@ -6,6 +6,13 @@ from typing import List, Optional, Tuple
 
 from ..db import database as db
 
+
+logging.basicConfig(
+    level=logging.DEBUG, 
+    format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
+
+
 async def _send_and_pin_message(bot: Bot, chat_id: int, text: Optional[str], photo_id: Optional[str], pin_message: bool, pin_silent: bool):
     """
     Отправляет фото и/или текст как отдельные сообщения.
